1a0293826a0eb30b256be02abafa3fa4bb65127a
Chris <chris.daf@gmail.com>
2019-06-13 17:04:41 +0100

    chapter 6 complete

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,18 +1,40 @@
 
-
 (function(exports) {
-    function NoteController() {
-        this.notelist = new NoteList();
-        this.notelistview = new NoteListView(this.notelist)
+    function NoteController(notelist) {
+        this.notelist = notelist;
+        this.notelist.addNote("Favourite drink: seltzer");
+        this.notelistview = new NoteListView(this.notelist);
     }
 
-    
+    NoteController.prototype.createNote = function(){
+        var list = this.notelistview.listNotes(this.notelist);
+        document.getElementById('app').textContent = list;
+    };
 
-    NoteController.prototype.showNote = function() {
-        var listnotes = new NoteListView()
-        var div = document.getElementById('app')
-        div.innerHTML = listnotes.listNotes();
-    }
+    // NoteController.prototype.showNote = function() {
+    //     var listnotes = new NoteListView()
+    //     var div = document.getElementById('app')
+    //     div.textContent = listnotes.listNotes();
+    // }
     exports.NoteController = NoteController;
+
+    window.onload = function(){
+
+        var body = document.getElementById('body');
+        body.style.background = '#33b5e5';
+        
+        var createButton = document.getElementById('create');
+                createButton.addEventListener('click', function(){
+                    var text_area = document.getElementById('textarea');
+                    var text = text_area.value;
+                    this.notelist.addNote(text);
+                });
+        
+        }
+
 })(this);
 
+
+var notes = new NoteList();
+var x = new this.NoteController(notes);
+x.createNote();
